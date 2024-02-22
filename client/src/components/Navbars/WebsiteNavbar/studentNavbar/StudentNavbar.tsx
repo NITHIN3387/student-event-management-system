bc8a11127b703a2a5e2d0fc8318a7146d094e412
spoorthi <spoorthiachar9@gmail.com>
2024-02-22 22:54:34 +0530

    navbar

--- client/src/components/Navbars/WebsiteNavbar/studentNavbar/StudentNavbar.tsx
+++ client/src/components/Navbars/WebsiteNavbar/studentNavbar/StudentNavbar.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
-<<<<<<< HEAD
 import sahyadriLogo from"@/assets/images/sahyadri-logo.png"
 const StudentNavbar: FC = (): JSX.Element => {
   return (
@@ -13,12 +12,6 @@ const StudentNavbar: FC = (): JSX.Element => {
      height={60}
      />
     </div>
-=======
-
-const StudentNavbar: FC = (): JSX.Element => {
-  return (
-    <div className='hidden md:flex justify-between pe-4 h-[5rem]  items-center bg-gray-900 '></div>
->>>>>>> spoorthi
   )
 }
 
