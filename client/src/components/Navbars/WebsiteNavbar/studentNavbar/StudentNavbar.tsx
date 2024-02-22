d8c75883133d274a3f0e6385e99517320a4b0f72
spoorthi <spoorthiachar9@gmail.com>
2024-02-22 18:43:45 +0530

    navbar

--- client/src/components/Navbars/WebsiteNavbar/studentNavbar/StudentNavbar.tsx
+++ client/src/components/Navbars/WebsiteNavbar/studentNavbar/StudentNavbar.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react'
 
+
 const StudentNavbar: FC = (): JSX.Element => {
   return (
-    <div>StudentNavbar</div>
+    <div className='hidden md:flex justify-between pe-4 h-[5rem]  items-center bg-gray-900 '></div>
   )
 }
 
