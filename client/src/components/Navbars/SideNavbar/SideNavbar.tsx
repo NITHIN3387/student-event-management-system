034e353bcdb9a541abecd34a2366f24dfff7b0d1
NITHIN3387 <nithinn9980@gmail.com>
2024-02-24 08:02:37 +0530

    some node packages are deleted

--- client/src/components/Navbars/SideNavbar/SideNavbar.tsx
+++ client/src/components/Navbars/SideNavbar/SideNavbar.tsx
@@ -3,7 +3,6 @@ import React, { FC } from "react";
 
 import Logo from"@/assets/images/sahyadri-logo.png"
 
-
 const SideNavbar: FC = (): JSX.Element => {
   return (
     <div className="hidden md:flex justify-between pe-4 h-[5rem]  items-center bg-gray-900 ">
