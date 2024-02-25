40effcee3a9355a679a669c2a02c5a5603bf4fe1
spoorthi <spoorthiachar9@gmail.com>
2024-02-25 23:20:46 +0530

    Participation card which shows the participation  details of the student

--- client/src/components/Cards/ParticipationCard/ParticipationCard.tsx
+++ client/src/components/Cards/ParticipationCard/ParticipationCard.tsx
@@ -1,9 +1,72 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
+import Image from 'next/image';
+import LocationLogo from "@/assets/images/location-pin.png"
+import PendingLogo from "@/assets/images/time.png"
+import DateLogo from "@/assets/images/calendar.png"
 
-const ParticipationCard: FC = (): JSX.Element => {
+const EventHistoryCard: FC = (): JSX.Element => {
   return (
-    <div>ParticipationCard</div>
-  )
+    <div className="border border-gray-300 p-4 rounded-lg shadow-md relative">
+      <div className="flex flex-col mb-4">
+        <ul className="space-y-2 flex-col">
+          <li>
+            <div className="flex justify-between flex-col">
+              <div>
+                <span className="font-semibold"></span> Event ABC
+              </div>
+              <div className="absolute top-4 right-4 bg-blue-500 rounded-3xl text-white p-2">
+                <Image
+                  src={PendingLogo}
+                  height={15}
+                  width={15}
+                  alt="logo"
+                  className="inline"
+                />
+                Pending
+              </div>
+              <div>
+                <span className="font-semibold">
+                  <Image
+                    src={LocationLogo}
+                    height={10}
+                    width={20}
+                    alt="logo"
+                    className="inline"
+                  />
+                </span> Participation Place XYZ
+              </div>
+              <div>
+                <span className="font-semibold">
+                  <Image
+                    src={DateLogo}
+                    height={10}
+                    width={20}
+                    alt="logo"
+                    className="inline"
+                  />
+                </span> 2022-02-28
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+      
+      <div className="flex justify-between items-end mt-auto">
+        <div className="flex items-center pl-4 pr-4 rounded-full  bg-yellow-500 cursor-pointer">
+          Update Certificate
+        </div>
+        
+        <div className="flex space-x-2">
+          <div className="pr-4 pl-4 ml-4 rounded-full border text-gray-500 border-gray-500 cursor-pointer">
+            Edit
+          </div>
+          <div className="pr-4 pl-4  rounded-full border text-red-500 border-red-500 cursor-pointer">
+            Delete
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ParticipationCard
+export default EventHistoryCard;
