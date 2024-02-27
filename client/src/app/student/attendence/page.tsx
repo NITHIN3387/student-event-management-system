d31068d01adb5262e26d17fbb012e6e73f0d8e90
NITHIN3387 <nithinn9980@gmail.com>
2024-02-27 23:02:29 +0530

    pages created for attendence and pendinng attendence

--- client/src/app/student/attendence/page.tsx
+++ client/src/app/student/attendence/page.tsx
@@ -1,9 +1,13 @@
+import AttendenceTable from '@/components/Tables/AttendenceTable/AttendenceTable'
 import React, { FC } from 'react'
 
 const page: FC = (): JSX.Element => {
   return (
     <div>
       Attendence
+      <div>
+        <AttendenceTable />
+      </div>
     </div>
   )
 }
