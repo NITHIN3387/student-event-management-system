da01154bd7d3bf463ee3f8bfcf69e9b6392fbbfc
NITHIN3387 <nithinn9980@gmail.com>
2024-02-29 11:46:22 +0530

    marks and attendence display added

--- client/src/app/student/attendence/page.tsx
+++ client/src/app/student/attendence/page.tsx
@@ -1,20 +1,52 @@
 "use client";
 
 import Table from "@/components/Tables/Table";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
+
+interface responceType {
+  CONDUCTED: number;
+  PRESENT: number;
+  SEMESTER: number;
+  SID: string;
+  SUBID: string;
+  SUBNAME: string;
+}
 
 const tableRowVal = [
   "Subject Name",
   "Total Class",
   "Present",
   "Absent",
-  "Percentage"
+  "Percentage",
 ];
 
 const Page: FC = (): JSX.Element => {
   const [tableColVal, setTableColVal] = useState<Array<Array<string | number>>>(
     [[]]
   );
+
+  useEffect(() => {
+    const fetchAttendence = async () => {
+      const url = process.env.NEXT_PUBLIC_SERVER_URL + "/attendence";
+
+      const responce: responceType[] = await fetch(url, {
+        method: "GET",
+        credentials: "include",
+      }).then((res) => res.json());
+
+      setTableColVal(
+        responce.map((subject) => [
+          subject.SUBNAME,
+          subject.CONDUCTED,
+          subject.PRESENT,
+          subject.CONDUCTED - subject.PRESENT,
+          (subject.PRESENT / subject.CONDUCTED) * 100,
+        ])
+      );
+    };
+
+    fetchAttendence();
+  }, []);
 
   return <Table tableRowVal={tableRowVal} tableColVal={tableColVal} />;
 };
--- client/src/app/student/marks/page.tsx
+++ client/src/app/student/marks/page.tsx
@@ -3,6 +3,17 @@
 import Table from "@/components/Tables/Table";
 import React, { useEffect, useState } from "react";
 
+interface responceType {
+  ASSIGNMENT: number;
+  IA1: number;
+  IA2: number;
+  IA3: number;
+  SEMESTER: number;
+  SID: string;
+  SUBID: string;
+  SUBNAME: string;
+}
+
 const tableRowVal = [
   "Subject Name",
   "IA-1",
@@ -18,8 +29,28 @@ const Page = () => {
   );
 
   useEffect(() => {
-    // api
-  }, [])
+    const fetchMarks = async () => {
+      const url = process.env.NEXT_PUBLIC_SERVER_URL + "/marks";
+
+      const responce: responceType[] = await fetch(url, {
+        method: "GET",
+        credentials: "include",
+      }).then((res) => res.json());
+
+      setTableColVal(
+        responce.map((subject) => [
+          subject.SUBNAME,
+          subject.IA1,
+          subject.IA2,
+          subject.IA3,
+          subject.ASSIGNMENT,
+          (subject.IA1 + subject.IA2 + subject.IA3) / 3 + subject.ASSIGNMENT
+        ])
+      );
+    };
+
+    fetchMarks();
+  }, []);
 
   return (
     <div>
