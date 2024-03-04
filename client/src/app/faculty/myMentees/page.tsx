9e0444330127a250e595ed64e194dbc02a67a18e
NITHIN3387 <nithinn9980@gmail.com>
2024-03-04 23:33:11 +0530

    mentees participation page created

--- client/src/app/faculty/myMentees/page.tsx
+++ client/src/app/faculty/myMentees/page.tsx
@@ -1,38 +1,35 @@
-"use client"
-import React,{FC,useState} from 'react'
-import '@/components/Headers/FilterHeader/FilterHeader'
-import FilterHeader from '@/components/Headers/FilterHeader/FilterHeader'
+"use client";
 
-interface ResponseType{
-  SID:string;
-  SNAME:string;
+import React, { FC, useState } from "react";
+import "@/components/Headers/FilterHeader/FilterHeader";
+import FilterHeader from "@/components/Headers/FilterHeader/FilterHeader";
+
+interface ResponseType {
+  SID: string;
+  SNAME: string;
 }
-const tableRowVal=[
-  "Student USN",
-  "Student Name",
+const tableRowVal = ["Student USN", "Student Name"];
 
-];
-
-const page :FC = ():JSX.Element => {
-  const[tableColVal,setTableColVal]=useState<Array<Array<string|number>>>(
+const Page: FC = (): JSX.Element => {
+  const [tableColVal, setTableColVal] = useState<Array<Array<string | number>>>(
     [[]]
   );
 
-  
-  const [semester,setSemester]=useState<number>(0);
-  const[section,setSection]=useState<string>("")
-  const[course,setCourse]=useState<string>(" ")
+  const [semester, setSemester] = useState<number>(0);
+  const [section, setSection] = useState<string>("");
+  const [course, setCourse] = useState<string>(" ");
   return (
-    <div className='mt-3'>
+    <div className="mt-3">
       <FilterHeader
-      semester={semester}
-      setSemester={setSemester}
-      section={section}
-      setSection={setSection}
-      course={course}
-      setCourse={setCourse}/>
+        semester={semester}
+        setSemester={setSemester}
+        section={section}
+        setSection={setSection}
+        course={course}
+        setCourse={setCourse}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
